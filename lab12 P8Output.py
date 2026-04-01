cb2f06b6e39fddbf9dfd05626509232054e92a72
Chandana-25D <chandanadbca25@rvu.edu.in>
2026-04-01 14:42:45 +0530

    Add files via upload

---
+++ lab12 P8Output.py
@@ -0,0 +1,11 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:\Users\Chandana D\AppData\Local\Programs\Python\Python314\lab12(4)P8.py
+Mean: 2.266666666666667
+Median: 2.2
+Mode: 3.2
+Minimum: 1.1
+Maximum: 3.2
+Variance: 0.7050000000000001
+Standard Deviation: 0.8396427811873333
